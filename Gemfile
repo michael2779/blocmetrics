5812cdd6f8d3b750c53e3ec979350c8f1115364e
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-08-15 14:52:39 -0700

    sendgrid

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'rspec-rails', '~> 3.0'
 
 gem 'faker'
 
+gem 'sendgrid-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,26 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'sendgrid-ruby'
+include SendGrid
+
+
+
+
+rom = Email.new(email: 'test@example.com')
+to = Email.new(email: 'test@example.com')
+subject = 'Sending with SendGrid is Fun'
+content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
+
+
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -31,8 +51,8 @@ module Blocmetrics
      :address  => 'smtp.mailgun.org',
      :domain   => 'sandboxbb675c83a8794d8e948e9fb8dada7de2.mailgun.org',
      :port     => 587,
-     :user_name  => ENV['MAILGUN_USERNAME'],
-     :password => ENV['MAILGUN_PASSWORD'],
+     :user_name  => ENV['SENDGRID_USERNAME'],
+     :password => ENV['SENDGRID_PASSWORD'],
      :authentication => ':plain',
    }
   end
